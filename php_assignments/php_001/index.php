3296822b9eb1f31d5428df9fbc085398b221b6f8
Ashraf Abed <ashraf.abed@acquia.com>
2016-11-30 19:13:51 -0500

    Update index.php

--- php_assignments/php_001/index.php
+++ php_assignments/php_001/index.php
@@ -1,5 +1,6 @@
 <?php
 
 // Replace this line with a variable named $my_name. Set it equal to your name.
+// For example: Ashraf would replace the line with the following: $my_name = 'Ashraf';
 
 echo "My name is " .  $my_name;
